b26eefe3a4af44bba73122685502982479c7216f
Jose Abreu <jose.abreu4@hotmail.com>
2026-05-08 21:14:57 -0700

    Adding manufacturer test suite

--- daos/manufacturer.js
+++ daos/manufacturer.js
@@ -34,8 +34,10 @@ export const updateOneManufacturerByCode = async (code, data) => {
 
 export const deleteOneManufacturerByCode = async (code) => {
   try {
+    console.log('SIII')
     return await Manufacturer.findOneAndDelete({ code: code.toUpperCase() });
   } catch (error) {
+    console.log('Que paso')
     throw new Error(`DAO Error (DeleteByCode): ${error.message}`);
   }
 };
